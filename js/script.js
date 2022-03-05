5529f5b2d2134c35ade8a90230a6fcb9d5ef9983
ramtaruman <sengupta.prakhar@gmail.com>
2022-03-05 19:23:14 +0530

    api fetching 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 let weather = {
     "apikey":"b37ac47ff7eb19f563b1be85e71a0eb1"
+    fetchWeather:function(){
+        fetch("http://api.openweathermap.org/data/2.5/weather?lat=22.4662&lon=88.4050&units=metric&appid=b37ac47ff7eb19f563b1be85e71a0eb1").then((response)=>response.json());
+    }
 }
