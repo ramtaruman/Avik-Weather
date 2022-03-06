21e9d2b05ed2aac510911202ff6859eb4f9179c7
ramtaruman <sengupta.prakhar@gmail.com>
2022-03-06 15:01:18 +0530

    Fixing api calls

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let weather = {
-    "apikey":"b37ac47ff7eb19f563b1be85e71a0eb1"
-    fetchWeather:function(){
-        fetch("http://api.openweathermap.org/data/2.5/weather?lat=22.4662&lon=88.4050&units=metric&appid=b37ac47ff7eb19f563b1be85e71a0eb1").then((response)=>response.json());
+    "apikey":"b37ac47ff7eb19f563b1be85e71a0eb1",
+    fetchWeather:function(city){
+        fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=b37ac47ff7eb19f563b1be85e71a0eb1")
+        .then((response)=>response.json())
+        .then((data)=>this.displayWeather(data));
+    },
+    displayWeather:function(data){
+        const{name}=data;
+        const{icon,description}=data.weather;
+        const{temp,humidity}=data.main;
+        const{speed}=data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
     }
-}
+    };
