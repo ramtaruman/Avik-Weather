f5bdd017a3cf929d65fef8121a0209e9ca201282
ramtaruman <sengupta.prakhar@gmail.com>
2022-03-09 18:23:02 +0530

    linking with frontend

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 let weather = {
     "apikey":"b37ac47ff7eb19f563b1be85e71a0eb1",
     fetchWeather:function(city){
-        fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=b37ac47ff7eb19f563b1be85e71a0eb1")
+        fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+this.apikey)
         .then((response)=>response.json())
         .then((data)=>this.displayWeather(data));
     },
     displayWeather:function(data){
         const{name}=data;
-        const{icon,description}=data.weather;
+        const{icon,description}=data.weather[0];
         const{temp,humidity}=data.main;
         const{speed}=data.wind;
         console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText="Weather in "+name;
+        document.querySelector(".icon").src="http://openweathermap.org/img/wn/"+icon+".png";
+        document.querySelector(".description").innerText=description;
+        document.querySelector(".temp").innerText=temp+"°C";
+        document.querySelector(".humidity").innerText="Humidity : "+humidity+" %";
     }
     };
